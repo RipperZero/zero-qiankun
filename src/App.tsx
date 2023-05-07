3e5cb5d91d4eb637dc0f9c3c79efb6bbdd1d670e
RipperZero <1018744707@qq.com>
2023-05-07 23:36:49 +0800

    feat: use vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-import { registerMicroApps, start } from "qiankun";
 import { FC } from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-registerMicroApps([
-  {
-    name: "reactApp",
-    entry: "//localhost:3000",
-    container: "#container",
-    activeRule: "/app-react",
-  },
-  {
-    name: "vueApp",
-    entry: "//localhost:8080",
-    container: "#container",
-    activeRule: "/app-vue",
-  },
-]);
-
-start();
+import { ErrorBoundary } from "./ErrorBoundary";
 
 const App: FC = () => {
-  return <div>App</div>;
+  return (
+    <ErrorBoundary>
+      <div>ROOT</div>
+    </ErrorBoundary>
+  );
 };
 
 export { App };
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // @see https://github.com/liuweiGL/vite-plugin-mkcert
+    mkcert({
+      source: "coding",
+    }),
+  ],
+  server: {
+    port: 9527,
+    host: true,
+    cors: true,
+    https: true,
+  },
+});
