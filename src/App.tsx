f9c8534960143804e659b98f2083575bcd830207
RipperZero <1018744707@qq.com>
2023-05-08 14:47:25 +0900

    feat: use vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-import { registerMicroApps, start } from "qiankun";
 import { FC } from "react";
+// import { BrowserRouter } from "react-router-dom";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-registerMicroApps([
-  {
-    name: "reactApp",
-    entry: "//localhost:3000",
-    container: "#container",
-    activeRule: "/app-react",
-  },
-  {
-    name: "vueApp",
-    entry: "//localhost:8080",
-    container: "#container",
-    activeRule: "/app-vue",
-  },
-]);
-
-start();
+import { ErrorBoundary } from "./ErrorBoundary";
+// import { Router } from "./Router";
 
 const App: FC = () => {
-  return <div>App</div>;
+  return (
+    <ErrorBoundary>
+      <div>ROOT</div>
+    </ErrorBoundary>
+  );
 };
 
 export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import { App } from "./App";
+import "./main.css";
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
